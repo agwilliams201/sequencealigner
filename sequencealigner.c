d8898043ef18348ed345608bbe7b771d7410bbf2
Alex Williams <alexwilliams@Alexs-MacBook-Air.local>
2023-11-02 00:23:35 -0500

    Starting SNPs Function

--- sequencealigner.c
+++ sequencealigner.c
@@ -1,3 +1,9 @@
-char* find_discrepancies(char* input, char* ref){
-    
+char* find_snps(char* input, char* ref, unsigned int len){
+    for (int i = 0; i < len; i++){
+        if (input[i] != ref[i]){
+            printf("There is a %c to %c mutation at BP %d\n", 
+            ref[i], input[i], i+1);
+        }
+    }
+    return;
 }
